ab3f80a2297c94b7938814cd0cf419f3a6a13f3b
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2020-01-31 08:44:35 +0900

    rename

--- server.py
+++ server.py
@@ -2,13 +2,13 @@
 
 from flask import Flask
 
-from src.libs import request_text_analysis
+from src.libs import text_analysis
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
-    sentence = request_text_analysis.sentence("pairsは年収600~　身長170より上　4大卒以上　禁煙者ってのが男性としての人権でどれか一つでもステータスがそこに達してないとないものとして扱われるんだなって気がしました。検索条件設定するから出てこないしね。いいねはたくさん来るわけだし")
+    sentence = text_analysis.sentence("pairsは年収600~　身長170より上　4大卒以上　禁煙者ってのが男性としての人権でどれか一つでもステータスがそこに達してないとないものとして扱われるんだなって気がしました。検索条件設定するから出てこないしね。いいねはたくさん来るわけだし")
     print(sentence)
     return sentence
 
