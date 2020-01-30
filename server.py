8ae0eaa5be15905989ab54b4d1c20b36b79c2fe3
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2020-01-30 19:52:38 +0900

    Yahooの文章解析APIでparseした

--- server.py
+++ server.py
@@ -4,20 +4,12 @@ from flask import Flask
 
 from src.libs import request_text_analysis
 
-import os
-from os.path import join, dirname
-from dotenv import load_dotenv
-
-load_dotenv(verbose=True)
-
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
-    sentence = request_text_analysis.sentence("aaaa")
+    sentence = request_text_analysis.sentence("pairsは年収600~　身長170より上　4大卒以上　禁煙者ってのが男性としての人権でどれか一つでもステータスがそこに達してないとないものとして扱われるんだなって気がしました。検索条件設定するから出てこないしね。いいねはたくさん来るわけだし")
+    print(sentence)
     return sentence
 
 if __name__ == "__main__":
--- src/libs/request_text_analysis.py
+++ src/libs/request_text_analysis.py
@@ -1,5 +1,19 @@
 import requests
+import xmltodict
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 def sentence(input_text):
-    r = requests.get('https://github.com/timeline.json')
-    return r.text
+    r = requests.post('https://jlp.yahooapis.jp/MAService/V1/parse',
+                      data={
+                          "appid": os.environ.get("YAHOO_API_KEY"),
+                          "results": "ma,uniq",
+                          "sentence": input_text,
+                      })
+    return xmltodict.parse(r.text)
